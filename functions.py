85ec004a47457f8688df5ce8a6c7810fa6fd5d92
Que5 <phumlanibrightdlamini@gmail.com>
2024-05-12 06:00:57 +0200

    Added a filepath function

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
-def get_todo(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todo(filepath=FILEPATH):
     with open(filepath, 'r') as file_file:
         todos_local = file_file.readlines() 
     return todos_local
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file_file:
         file_file.writelines(todos_arg) 
   
