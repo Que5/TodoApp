ad661777efea97739a3c1b7d0d0c071fcbd866fa
Que5 <phumlanibrightdlamini@gmail.com>
2024-05-18 13:17:43 +0200

    Added interactivity

--- web.py
+++ web.py
@@ -1,7 +1,16 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos()
+
+
+todos = functions.get_todos() 
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 
 st.title("My Todo App")
 st.subheader("This is a simple to do app")
@@ -9,4 +18,4 @@ st.write("This app is to increase your productivity")
 
 for todo in todos:
     st.checkbox(todo)
-st.text_input(label="", placeholder="Add a new todo:")
+st.text_input(label="", placeholder="Add a new todo:", on_change=add_todo, key='new_todo')
