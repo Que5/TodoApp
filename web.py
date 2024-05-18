f699a8863f49b3620b9c0b92b0ace2742c28f702
Que5 <phumlanibrightdlamini@gmail.com>
2024-05-18 13:34:27 +0200

    Added a delete option

--- web.py
+++ web.py
@@ -2,7 +2,6 @@ import streamlit as st
 import functions
 
 
-
 todos = functions.get_todos() 
 
 def add_todo():
@@ -11,11 +10,15 @@ def add_todo():
     functions.write_todos(todos)
 
 
-
 st.title("My Todo App")
 st.subheader("This is a simple to do app")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 st.text_input(label="", placeholder="Add a new todo:", on_change=add_todo, key='new_todo')
